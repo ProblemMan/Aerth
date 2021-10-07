edc69a83a9a6e15cfc40b2676aa24e3841641a0e
19175 <73862885+19175@users.noreply.github.com>
2021-10-08 04:31:08 +1300

    Recipes
    More recipes, made machine frame craftable and messed with tinkers smeltry functions

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -1,8 +1,10 @@
 // priority: 1
+
 onEvent('recipes', r => {
     //Removals
     r.remove({input: 'waystones:warp_stone'})
 
+    r.remove({output: 'thermal:obsidian_glass'})
     //Additions
 
     //waystones
@@ -59,24 +61,68 @@ onEvent('recipes', r => {
         S: 'appliedenergistics2:singularity'
     })
 
-    //pure seeds
+    //seeds //TODO: Add others, finalise numbers (after augments added)
+    r.recipes.thermal.insolator('appliedenergistics2:purified_certus_quartz_crystal', 'appliedenergistics2:certus_crystal_seed').water(64000).energyMod(10.0)
     
-    let seedGrow = (crystal, seed) => {
-        let seed_stages = [60, 120, 180, 240, 300, 360, 420, 480, 540]
-
-        r.recipes.thermal.insolator(Item.of(seed, `{p:${seed_stages[1]}}`), seed).water(32000)
-
-        for (let i in seed_stages) {
-            if (i < 9){
-                r.recipes.thermal.insolator(Item.of(seed, `{p:${seed_stages[i] + 60}}`), Item.of(seed, `{p:${seed_stages[i]}}`)).water(1000)
-            }
-        }
-        r.recipes.thermal.insolator(crystal, Item.of(seed, `{p:${seed_stages[seed_stages.length - 1]}}`)).water(1000)
-
+    let tinkersCastingTable = (output, cool_ticks, input_fluid, fluid_mb, input_item) => {
+        r.custom({
+            type: "tconstruct:casting_basin",
+            fluid: {
+              name: input_fluid,
+              amount: fluid_mb
+            },
+            cast: input_item,
+            cast_consumed: true,
+            result: output,
+            cooling_time: cool_ticks
+          })
     }
-    seedGrow('appliedenergistics2:purified_certus_quartz_crystal', 'appliedenergistics2:certus_crystal_seed')
-    
-
-    
-    
+    let tinkersComplexAlloy = (output, output_mb, temperature, inputA, inputA_mb, inputB, inputB_mb, inputC, inputC_mb) => {
+        r.custom({
+        type: "tconstruct:alloy",
+        inputs: [
+          {
+            name: inputA,
+            amount: inputA_mb
+          },
+          {
+            name: inputB,
+            amount: inputB_mb
+          },
+          {
+            name: inputC,
+            amount: inputC_mb
+          }
+        ],
+        result: {
+          fluid: output,
+          amount: output_mb
+        },
+        temperature: temperature
+      })
+    }
+    let tinkersSimpleAlloy = (output, output_mb, temperature, inputA, inputA_mb, inputB, inputB_mb) => {
+        r.custom({
+        type: "tconstruct:alloy",
+        inputs: [
+          {
+            name: inputA,
+            amount: inputA_mb
+          },
+          {
+            name: inputB,
+            amount: inputB_mb
+          },
+        ],
+        result: {
+          fluid: output,
+          amount: output_mb
+        },
+        temperature: temperature
+      })
+    }
+    tinkersSimpleAlloy('kubejs:molten_hardened_glass', 100, 500, 'tconstruct:molten_glass', 100, 'tconstruct:molten_obsidian', 50)
+    tinkersCastingTable('thermal:machine_frame', 200, 'kubejs:molten_hardened_glass', 4000, '#forge:gears/quartz')
+    r.recipes.thermal.press('thermal:machine_frame', '#forge:gears/quartz', Item.of('4x thermal:obsidian_glass'))
+    tinkersCastingTable('thermal:obsidian_glass', 100, 'kubejs:molten_hardened_glass', 1000)
 })
--- kubejs/startup_scripts/creation.js
+++ kubejs/startup_scripts/creation.js
@@ -8,4 +8,5 @@ onEvent('block.registry', event => {
 
 onEvent('fluid.registry', event => {
 	event.create('molten_singularity').textureThick(0x21038C).bucketColor(0x21038C).displayName('Gooey Singularity')
+    event.create('molten_hardened_glass').textureThin(0x7d8599).displayName('Molten Hardened Glass')
 })
